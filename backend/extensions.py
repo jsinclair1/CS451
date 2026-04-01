2e009b440e91aabaab60723129dc4e6cef7171d8
jsinclair1 <123492773+jsinclair1@users.noreply.github.com>
2026-03-31 19:32:36 -0500

    Add JWTManager

--- backend/extensions.py
+++ backend/extensions.py
@@ -3,9 +3,11 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
 bcrypt = Bcrypt()
 cors = CORS()
+jwt = JWTManager()
